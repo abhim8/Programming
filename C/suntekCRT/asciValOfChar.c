3e5e983de562ac4a699f65275dae75a950835476
Abhilash Movva <abhilash.movva@gmail.com>
2021-08-26 11:09:38 +0530

    Delete C directory

--- C/suntekCRT/asciValOfChar.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    char c; 
-    printf("Enter a Character: "); scanf("%c",&c);
-    printf("Ascii value is : %d\n",c);
-    return 0;
-}
--- C/suntekCRT/evenorOdd.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>  
-int main(){   
-    int a;
-    printf("Enter a number: ");
-    scanf("%d",&a);
-    a & 1 ? printf("\nnumber is Odd\n") : printf("\nnumber is Even\n");
-    return 0;  
-}  
